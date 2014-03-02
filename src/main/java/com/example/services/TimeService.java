ac20cc0defb06ac85ab2b1dc96baa2a7faa2f69f
Gervasio Junior <juniorb08@gmail.com>
2014-03-02 17:24:40 +0000

    adding timezone support

--- src/main/java/com/example/services/TimeService.java
+++ src/main/java/com/example/services/TimeService.java
@@ -1,9 +1,12 @@
 package com.example.services;
 
+import java.util.TimeZone;
+
 import com.example.models.Time;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -14,6 +17,12 @@ public class TimeService {
     @GET
     public Time get() {
         return new Time();
+    }
+    
+    @GET
+    @Path("/{timezone}")
+    public Time get(@PathParam("timezone") String timezone) {
+    	return new Time(TimeZone.getTimeZone(timezone.toUpperCase()));
     }
 
 }
