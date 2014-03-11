b9467098068dc44cfdfe7e402c04e131210ec8ac
Stephen Hanley <steoh@hotmail.co.uk>
2014-03-11 01:02:00 +0000

    added createEntry method to CalendarDAO.java

--- src/main/java/com/example/controller/CalendarDAO.java
+++ src/main/java/com/example/controller/CalendarDAO.java
@@ -1,15 +1,18 @@
 package com.example.controller;
 
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
 import com.example.models.*;
+import com.example.models.Entry.Repetition;
 
 public class CalendarDAO {
 
 	private final static Map<Integer, HatsuCalendar> calendars = new HashMap<Integer, HatsuCalendar>();
+	private final static Map<Integer, Entry> entry = new HashMap<Integer, Entry>();
 	
 	public static Collection<HatsuCalendar> getAllCalendars() {
 		return calendars.values();
@@ -19,4 +22,11 @@ public class CalendarDAO {
 		int cal = new Random().nextInt();
 		return calendars.put(cal, new HatsuCalendar(owner));
 	}
+	
+	public static Entry createEntry(String desc, Calendar start, Calendar end, Repetition rep, int repeats, String loc) {
+		int randNum = new Random().nextInt();
+		return entry.put(randNum, new Entry(desc,start,end,rep,repeats,loc));
+		
+	}
+	
 }
